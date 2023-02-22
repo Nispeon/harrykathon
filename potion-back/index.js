e7a3836215a4d4ecb5ff62a234833a91e604417f
Nicolas de Garrigues <ndegarrigues@gmail.com>
2023-02-22 11:36:51 +0100

    :sparkles: FEAT add cors

--- potion-back/index.js
+++ potion-back/index.js
@@ -4,6 +4,8 @@ import router from './router.js'
 import sequelize from './database.js'
 import cors from 'cors'
 import morgan from 'morgan'
+import { Server } from 'socket.io'
+import { createServer } from 'http'
 
 // models
 import './models/models.js'
@@ -17,15 +19,30 @@ sequelize
 		console.log('Connection has been established successfully.')
 
 		app.use(cors())
-
-    app.use('/api', router)
-
 		app.use(bodyParser.json())
 		app.use(morgan('dev'))
 
+		app.use('/api', router)
+
+    const http = createServer(app)
+
+		const io = new Server(http, {
+			cors: {
+				origin: '*',
+			},
+			transports: ['websocket', 'polling'],
+		})
+
+		io.on('connection', (socket) => {
+			console.log('a user connected')
+			socket.on('disconnect', () => {
+				console.log('user disconnected')
+			})
+		})
+
 		sequelize.sync()
 
-		app.listen(port, () => {
+		http.listen(port, () => {
 			console.log(`Server listening at port: ${port}`)
 		})
 	})
