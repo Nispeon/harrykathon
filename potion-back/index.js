3cf380fd007e156d067ec53330ce3805b054199c
Nicolas de Garrigues <ndegarrigues@gmail.com>
2023-02-22 09:14:11 +0100

    :sparkles: FEAT set sequelize

--- potion-back/index.js
+++ potion-back/index.js
@@ -1,15 +1,32 @@
-import { Server } from 'socket.io'
+import express from 'express'
+import bodyParser from 'body-parser'
+import sequelize from './database.js'
+import cors from 'cors'
+import morgan from 'morgan'
 
-const io = new Server({
-	cors: {
-		origin: '*',
-	},
-})
+// models
+import Plant from './models/plant.js'
+import Spell from './models/spell.js'
 
-io.on("connection", (socket) => {
-  console.log("connected")
-})
+const app = express()
+const port = 3000
 
-io.listen(3000, () => {
-  console.log(`listening on port 3000`)
-})
+sequelize
+	.authenticate()
+	.then(() => {
+		console.log('Connection has been established successfully.')
+
+		app.use(cors())
+
+		app.use(bodyParser.json())
+		app.use(morgan('dev'))
+
+		sequelize.sync()
+
+		app.listen(port, () => {
+			console.log(`Server listening at port: ${port}`)
+		})
+	})
+	.catch((err) => {
+		console.error('Unable to connect to the database:', err)
+	})
