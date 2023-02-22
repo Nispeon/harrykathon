e2cc8985887b5618181b09e44ebb160101d502f9
Bakabruh <chewie.louveau@gmail.com>
2023-02-22 10:00:46 +0100

    start docket

--- potion-back/index.js
+++ potion-back/index.js
@@ -1,15 +1,48 @@
+import express from 'express'
+import router from './api/router.js'
+import bodyParser from 'body-parser'
+import database from './database.js'
+import cors from 'cors'
 import { Server } from 'socket.io'
+import * as dotenv from 'dotenv'
+import { createServer } from 'http'
 
-const io = new Server({
-	cors: {
-		origin: '*',
-	},
+dotenv.config({
+  path: './.env.local',
+  override: true
 })
 
-io.on("connection", (socket) => {
-  console.log("connected")
+const app = express()
+const port = process.env.PORT
+
+database.connect()
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use('/api', router)
+
+const httpServer = createServer(app)
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
+  transports: ['websocket', 'polling'],
 })
 
-io.listen(3000, () => {
-  console.log(`listening on port 3000`)
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+  socket.on('tweet', (tweet) => {
+    io.emit('tweet', tweet)
+  })
+  socket.on('hashtags', (hashtags) => {
+    io.emit('hashtags', hashtags)
+  })
+})
+
+httpServer.listen(port, () => {
+  console.log(`Server running on port ${port}`)
 })
