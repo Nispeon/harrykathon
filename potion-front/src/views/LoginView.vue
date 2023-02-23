6ba2c026be09a1d4d7d45fc71db54bf2250a42b0
Quintin Florent <55179969+Boloblux@users.noreply.github.com>
2023-02-23 11:47:55 +0100

    style : page de login

--- potion-front/src/views/LoginView.vue
+++ potion-front/src/views/LoginView.vue
@@ -1,18 +1,23 @@
 <template>
-	<div>
-		<form @submit.prevent="login">
-			<input
-				type="text"
-				placeholder="Username"
-				v-model="form.name"
-			/>
-			<input
-				type="password"
-				placeholder="Password"
-				v-model="form.password"
-			/>
-			<button type="submit">Login</button>
-		</form>
+	<div class="loginContainer">
+		<div class="formWrapper">
+			<form @submit.prevent="login">
+				<h1>Login</h1>
+				<div class="inputContainer">
+					<input
+						type="text"
+						placeholder="Username"
+						v-model="form.name"
+					/>
+					<input
+						type="password"
+						placeholder="Password"
+						v-model="form.password"
+					/>
+				</div>
+				<button type="submit">Login</button>
+			</form>
+		</div>
 	</div>
 </template>
 <script>
@@ -48,4 +53,56 @@
 		}
 	}
 </script>
-<style></style>
+<style scopped>
+* {
+	font-family: 'ringbearermedium';
+	color: #2c3e50;
+}
+
+.loginContainer {
+	width: 100vw;
+	height: 100vh;
+	background-color: black;
+	display: flex;
+	align-items: center;
+}
+
+.formWrapper {
+	margin: auto;
+	width: 500px;
+	background: -webkit-linear-gradient(top,#7579ff,#b224ef);
+	padding: 55px 55px 37px;
+	border-radius: 10px;
+}
+
+form {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	gap: 40px;
+}
+
+.inputContainer {
+	width: 80%;
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
+}
+
+input {
+	border-radius: 5px;
+	height: 40px;
+	font-size: 16px;
+	padding: 0 10px;
+	border: none;
+}
+
+button {
+	font-size: 16px;
+	background-color: white;
+	border: none;
+	border-radius: 25px;
+	padding: 10px 20px;
+}
+</style>
